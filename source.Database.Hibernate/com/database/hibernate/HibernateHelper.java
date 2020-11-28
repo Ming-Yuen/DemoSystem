becfdfefa6da60d5dc191c59de076e012d255e74
Ming-yuen <toyuenkaming@gmail.com>
2020-11-28 13:29:45 +0800

    hibernate, set hibernat.cfg.xml path

---
+++ source.Database.Hibernate/com/database/hibernate/HibernateHelper.java
@@ -0,0 +1,89 @@
+package com.database.hibernate;
+
+import java.io.File;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.common.util.ClassUtil;
+
+public class HibernateHelper {
+	private static SessionFactory factory;
+
+	static {
+		File f = new File("C:\\Users\\Admin\\eclipse-workspace\\DemoSystemBackEnd\\source.Database.Hibernate\\com\\hibernate\\configuration\\hibernate.cfg.xml");
+		factory = new Configuration().configure(f).buildSessionFactory();
+	}
+	
+	
+
+	public static String addObject(Object record) {
+		String value = null;
+		try (Session session = factory.openSession()) {
+			Transaction tx = session.beginTransaction();
+			value = String.valueOf(session.save(record));
+			tx.commit();
+		}
+		return value;
+	}
+
+	public static List<?> query(String queryString) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		boolean isProcess = false;
+		try {
+			tx = session.beginTransaction();
+			List<?> resultSet = session.createQuery(queryString).list();
+			tx.commit();
+			isProcess = true;
+			return resultSet;
+		} finally {
+			if (!isProcess && tx != null) {
+				tx.rollback();
+			}
+			session.close();
+		}
+	}
+
+	public void updateEmployee(Object updateRecord, Integer id, String fieldName, Object salary) throws Exception {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		boolean isProcess = false;
+
+		try {
+			tx = session.beginTransaction();
+			Object dbRecord = session.get(updateRecord.getClass(), ClassUtil.<Integer>getFieldValue(updateRecord, "id"));
+			ClassUtil.referenceFields(updateRecord, dbRecord);
+			session.update(dbRecord);
+			tx.commit();
+			isProcess = true;
+		} finally {
+			if (!isProcess && tx != null) {
+				tx.rollback();
+			}
+			session.close();
+		}
+	}
+
+	/* Method to DELETE an employee from the records */
+	public void deleteEmployee(Class<?> clazz, Integer id) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		boolean isProcess = false;
+		try {
+			tx = session.beginTransaction();
+			Object employee = session.get(clazz, id);
+			session.delete(employee);
+			tx.commit();
+			isProcess = true;
+		} finally {
+			if (!isProcess && tx != null) {
+				tx.rollback();
+			}
+			session.close();
+		}
+	}
+}
--- source.service/com/container/Container.java
+++ source.service/com/container/Container.java
@@ -6,6 +6,7 @@ import com.database.DatabaseRecordHelper;
 import com.database.DatabaseRecordRebuild;
 import com.database.hibernate.HibernateHelper;
 import com.database.hibernate.model.ProductsModel;
+import com.database.hibernate.testcase.hibernate;
 import com.database.test.Products;
 import com.global.Global;
 import com.google.gson.Gson;
@@ -39,7 +40,7 @@ public class Container extends HttpServlet {
 			try {
 				dbConn = DatabaseHelper.getConnection();
 				systemPath = getServletContext().getRealPath("");
-				test(dbConn);
+				test();
 				isProcessSuccess = true;
 			} catch (Exception e) {
 				Global.getLogger.error(getClass().getName(), e.getMessage(), e);
@@ -58,45 +59,12 @@ public class Container extends HttpServlet {
 		}
 	}
 	
+	public void test() throws Exception {
+		hibernate.test();
+	}
+	
 	public static String getSystemPath() {
 		return systemPath;
-	}
-	
-	public void baidu() throws IOException {
-		String url = "https://tieba.baidu.com/p/6656185856";
-		String uri2 = "https://tieba.baidu.com/f/commit/post/add";
-		HttpConnection.Mandatory mandatory = new HttpConnection.Mandatory();
-		mandatory.setApiUrl(url);
-		mandatory.setContentType(MediaType.APPLICATION_JSON);
-		mandatory.setMethod(HttpMethod.POST);
-		HttpConnection http = new HttpConnection(mandatory, null);
-		System.out.println(http.connect().getContent());
-	}
-	
-	public void test(Connection dbConn) throws Exception {
-		ProductsModel model = new ProductsModel();
-		model.setId(UUID.randomUUID().toString());
-		model.setProductCode("Test");
-		model.setProductName("test name");
-
-		model.setProductLine("line");
-
-		model.setProductScale("scale");
-
-		model.setProductVendor("vendor");
-
-		model.setProductDescription("desc");
-
-		model.setQuantityInStock(10);
-
-		model.setBuyPrice(new BigDecimal(10));
-
-		model.setMSRP(new BigDecimal(10));
-		model.setModifytime(new java.sql.Date(new Date().getTime()));
-		HibernateHelper.addObject(model);
-//		List<?> result = HibernateHelper.query("select productName, productLine FROM ProductsModel where productName = '1969 Harley Davidson Ultimate Chopper'");
-//		System.out.println(new Gson().toJson(result));
-		
 	}
 
 	public void dbProcess() throws SQLException {
